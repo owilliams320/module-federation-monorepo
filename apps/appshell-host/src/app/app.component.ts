ae4e1db8115cc4bc182ea3885ff60928ab433742
oseer <oseer.williams@gmail.com>
2022-10-20 11:32:19 -0400

    Initial commit

---
+++ apps/appshell-host/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'module-federation-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'appshell-host';
+}
